6823923837d8866455f96200ec8ec8bb598e2343
Lucas Duarte <lucashdp@outlook.com>
2019-03-10 23:43:23 -0300

    Renomeia arquivo de execucao java e pasta duplicada

--- src/main/java/com/javaee/mercadoacoes/MercadoAcoesApplication.java
+++ src/main/java/com/javaee/mercadoacoes/MercadoAcoesApplication.java
@@ -1,13 +1,13 @@
-package com.mercadoacoes.mercadoacoes;
+package com.javaee.mercadoacoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MercadoAcoesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MercadoAcoesApplication.class, args);
 	}
 
 }
